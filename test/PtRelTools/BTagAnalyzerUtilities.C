39d3bcc6dca68d7c879e2fb072e30a2a211e2106
scodella <luca.scodellaro@cern.ch>
2021-06-08 11:38:43 +0200

    Fix JEU for UL16 non-APV RunF

--- test/PtRelTools/BTagAnalyzerUtilities.C
+++ test/PtRelTools/BTagAnalyzerUtilities.C
@@ -1399,9 +1399,10 @@ void ReadJetEnergyUncertainty(TString DataType, TString CorrectionVersion, TStri
     TString tok; Ssiz_t from = 0; const char* delim = "_";
     while (DataRangeName.Tokenize(tok, from, delim)) {
       if (tok.Contains("Run")) {
-        if (CorrectionVersion.Contains("Summer19UL16")) {
+        if (CorrectionVersion.Contains("Summer19UL16APV")) {
           if (tok.Contains("B") or tok.Contains("C") or tok.Contains("D"))      JEUFileName.ReplaceAll("RunXXX", "RunBCD");
           else if (tok.Contains("E") or tok.Contains("F"))                      JEUFileName.ReplaceAll("RunXXX", "RunEF");
+        } else if (CorrectionVersion.Contains("Summer19UL16")) {
           else if (tok.Contains("F") or tok.Contains("G") or tok.Contains("H")) JEUFileName.ReplaceAll("RunXXX", "RunFGH");
         } else {
           if (tok.Contains("A"))   JEUFileName.ReplaceAll("RunXXX", "RunA");
